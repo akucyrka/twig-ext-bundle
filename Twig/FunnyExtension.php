0a2714ad4da9c5e0d8e25948158fdf2af62e4f8f
Adrian Kucyrka <akucyrka@ioki.com.pl>
2013-03-13 14:48:21 +0100

    Update - add  thinkFunctiong:wq

--- Twig/FunnyExtension.php
+++ Twig/FunnyExtension.php
@@ -21,12 +21,18 @@ class FunnyExtension extends \Twig_Extension
     {
         return array(
             'smile' => new \Twig_Function_Method($this, 'smileFunction'),
+            'think' => new \Twig_Function_Method($this, 'thinkFunction'),
         );
     }
 
     public function smileFunction($number = 5)
     {
         return str_repeat(':-)', $number);
+    }
+
+    public function thinkFunction()
+    {
+        return '--- in progress ---';
     }
 
     public function getName()
